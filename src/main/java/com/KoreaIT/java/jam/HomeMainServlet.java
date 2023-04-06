46005f3beba4234e2edb22d022eeb2ff5983810f
SeoDongYeon1 <ehddus2655@gmail.com>
2023-04-06 14:57:51 +0900

    구구단 파라미터 사용

--- src/main/java/com/KoreaIT/java/jam/HomeMainServlet.java
+++ src/main/java/com/KoreaIT/java/jam/HomeMainServlet.java
@@ -1,7 +1,6 @@
 package com.KoreaIT.java.jam;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,23 +8,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/printDan/dan") // 요청사항에 대한 주소 정의
+@WebServlet("/printDan") // 요청사항에 대한 주소 정의
 public class HomeMainServlet extends HttpServlet {
-       
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		response.setContentType("text/html;charset=UTF-8"); // html문법을 써서 웹페이지 한글 깨짐 해결
 
-		response.getWriter().append("구구단<br>");
-				
-		for(int dan = 2; dan <= 9; dan++) {
-			response.getWriter().append("=====" + dan + "단====="+"<br/>");
-			for(int j = 1; j<=9; j++) {
-				response.getWriter().append(dan + " * " + j + " = " + dan*j + "<br/>");
-			}
+		String inputedDan = request.getParameter("dan");
+		String inputedLimit = request.getParameter("limit");
+		
+		if(inputedDan==null) {
+			inputedDan = "1";
 		}
 		
-//		PrintWriter out = response.getWriter();
-//		out.print("이름: " + "<br/>");
-//		out.print("비밀번호: " + "<br/>");
+		if(inputedLimit==null) {
+			inputedLimit = "1";
+		}
+		
+		int dan = Integer.parseInt(inputedDan);
+		int limit = Integer.parseInt(inputedLimit);
+
+		response.getWriter().append(String.format("==%d단==<br>", dan));
+
+		for (int i = 1; i <= limit; i++) {
+			response.getWriter().append(String.format("%d * %d = %d<br>", dan, i, dan*i));
+		}
 	}
+
 }
