3e0ca218775a45ad832e0af7c6808c6c534a0afc
SeoDongYeon1 <ehddus2655@gmail.com>
2023-04-06 15:26:54 +0900

    파라미터에 따라서 텍스트의 색상 변경

--- src/main/java/com/KoreaIT/java/jam/HomeMainServlet.java
+++ src/main/java/com/KoreaIT/java/jam/HomeMainServlet.java
@@ -1,39 +1,16 @@
 package com.KoreaIT.java.jam;
 
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/printDan") // 요청사항에 대한 주소 정의
+@WebServlet("/home/main2") // 요청사항에 대한 주소 정의
 public class HomeMainServlet extends HttpServlet {
-
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		response.setContentType("text/html;charset=UTF-8"); // html문법을 써서 웹페이지 한글 깨짐 해결
-
-		String inputedDan = request.getParameter("dan");
-		String inputedLimit = request.getParameter("limit");
-		
-		if(inputedDan==null) {
-			inputedDan = "1";
-		}
-		
-		if(inputedLimit==null) {
-			inputedLimit = "1";
-		}
-		
-		int dan = Integer.parseInt(inputedDan);
-		int limit = Integer.parseInt(inputedLimit);
-
-		response.getWriter().append(String.format("==%d단==<br>", dan));
-
-		for (int i = 1; i <= limit; i++) {
-			response.getWriter().append(String.format("%d * %d = %d<br>", dan, i, dan*i));
-		}
+       
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Hello, World!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!").append(request.getContextPath());
 	}
-
 }
