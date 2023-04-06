dc665b6dbafbfd6190e02c3cf9ea970213eceabd
SeoDongYeon1 <ehddus2655@gmail.com>
2023-04-06 14:39:06 +0900

    구구단 출력, 파라미터는 아직

--- src/main/java/com/KoreaIT/java/jam/HomeMainServlet.java
+++ src/main/java/com/KoreaIT/java/jam/HomeMainServlet.java
@@ -1,16 +1,31 @@
 package com.KoreaIT.java.jam;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet("/home/main") // 요청사항에 대한 주소 정의
+@WebServlet("/printDan/dan") // 요청사항에 대한 주소 정의
 public class HomeMainServlet extends HttpServlet {
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("안녕").append(request.getContextPath());
+		response.setContentType("text/html;charset=UTF-8"); // html문법을 써서 웹페이지 한글 깨짐 해결
+
+		response.getWriter().append("구구단<br>");
+				
+		for(int dan = 2; dan <= 9; dan++) {
+			response.getWriter().append("=====" + dan + "단====="+"<br/>");
+			for(int j = 1; j<=9; j++) {
+				response.getWriter().append(dan + " * " + j + " = " + dan*j + "<br/>");
+			}
+		}
+		
+//		PrintWriter out = response.getWriter();
+//		out.print("이름: " + "<br/>");
+//		out.print("비밀번호: " + "<br/>");
 	}
 }
